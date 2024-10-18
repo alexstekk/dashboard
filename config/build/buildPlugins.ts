c9bdce61fb21a6c5446232a3e370408e67a8329e
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-10-18 13:42:26 +0300

    gitignore fix

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,28 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ];
+
+    if (isDev) {
+        plugins.push(
+            new ReactRefreshWebpackPlugin(),
+        );
+    }
+
+    return plugins;
+}
