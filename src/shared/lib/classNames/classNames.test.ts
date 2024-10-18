321292c8b2005e01c8fc4756a680279c1a0cb6be
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-10-18 13:37:57 +0300

    add jest env

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,11 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('Only one arg', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('All args', () => {
+        const expected = 'someClass addClass red';
+        expect(classNames('someClass', { red: true }, ['addClass'])).toBe(expected);
+    });
+});
